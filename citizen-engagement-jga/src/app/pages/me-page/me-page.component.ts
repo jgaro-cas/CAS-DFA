a2f13b945130b574cbc1dccfc11b7a1681a0e905
Jérôme Garo <jerome.garo@gmail.com>
2020-09-04 20:39:30 +0200

    form qui fonctionne

--- citizen-engagement-jga/src/app/pages/me-page/me-page.component.ts
+++ citizen-engagement-jga/src/app/pages/me-page/me-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-me-page',
   templateUrl: './me-page.component.html',
   styleUrls: ['./me-page.component.scss']
+ 
 })
 export class MePageComponent implements OnInit {
+
+  meForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(meDatas){
+
+  }
 }
--- citizen-engagement-jga/src/app/pages/me-page/me-page.module.ts
+++ citizen-engagement-jga/src/app/pages/me-page/me-page.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MePageComponent } from './me-page.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-
+import { FormsModule} from '@angular/forms';
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [MePageComponent],
   imports: [
     CommonModule,
+    FormsModule,
     MatButtonModule,
-    MatFormFieldModule
-
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [MePageComponent]
 })
